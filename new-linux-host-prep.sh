1698a07ea1dcd3ab685030ad167487e94c7919b0
Trevor Putbrese <trevor.putbrese@gmail.com>
2021-05-23 12:32:17 -0400

    Update new-linux-host-prep.sh

--- new-linux-host-prep.sh
+++ new-linux-host-prep.sh
@@ -1,2 +1,33 @@
+#!/usr/bin/env bash
+#
+#install open-ssh
 sudo apt update
 sudo apt upgrade
+sudo apt install openssh-server -y
+sudo systemctl enable ssh
+sudo systemctl start ssh
+sudo ufw allow ssh
+#
+#install RDP
+sudo apt install xrdp -y
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
+sudo ufw allow 3389
+#
+#install docker
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
