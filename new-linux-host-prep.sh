db2c401ddcc9c83d5e1674d49f9ee53712376617
Trevor Putbrese <trevor.putbrese@gmail.com>
2021-05-23 15:46:41 -0400

    Update new-linux-host-prep.sh

--- new-linux-host-prep.sh
+++ new-linux-host-prep.sh
@@ -2,7 +2,7 @@
 #
 #install open-ssh
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install openssh-server -y
 sudo systemctl enable ssh
 sudo systemctl start ssh
@@ -21,7 +21,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
