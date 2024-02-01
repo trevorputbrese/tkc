999f1f1fab126bd4a74751b78b7d72b46d4caaec
Trevor Putbrese <trevor.putbrese@gmail.com>
2024-02-01 15:47:11 -0500

    Commented out unneeded kubeconfig line

--- ssh-to-tkc-node.sh
+++ ssh-to-tkc-node.sh
@@ -9,7 +9,7 @@ sshkey=$workdir/gc-sshkey # path for gc private key
 gckubeconfig=$workdir/kubeconfig # path for gc kubeconfig
 kubectl get secret -n $gcnamespace $gcname"-ssh" -o jsonpath='{.data.ssh-privatekey}' | base64 --decode > $sshkey
 chmod 600 $sshkey
-kubectl get secret -n $gcnamespace $gcname"-kubeconfig" -o jsonpath='{.data.value}' | base64 --decode > $gckubeconfig
+# kubectl get secret -n $gcnamespace $gcname"-kubeconfig" -o jsonpath='{.data.value}' | base64 --decode > $gckubeconfig
 #
 echo "Enter the target IP address of the TKC node"
 read tkcip
